d79e6df3c8fb885e2c1eddcb4b9836093d11bd81
Adrian <adrianqueizan@gmail.com>
2023-08-04 11:41:06 -0400

    modifies objects

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+   return { ...obj,[key]: value, }
+}    
+
+const newEmployee = updateEmployeeWithKeyAndValue(employee,streetAddress,"11 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.streetAddress = "12 Broadway"
+    return employee
+}
+
+function deleteFromEmployeeByKey(obj){
+   const newobj = {...obj}
+   delete newobj.name
+   return newobj
+}
+
+function  destructivelyDeleteFromEmployeeByKey(obj){
+    const newobj = obj
+    delete newobj.name
+    return newobj
+}
